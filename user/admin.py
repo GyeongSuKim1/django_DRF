b51ac2a4ac7137ec5a02eb85ac478cdd35b5e1fd
GyeongSuKim <rla04722@naver.com>
2022-06-20 19:52:47 +0900

    Feat : 커스텀 어드민

--- user/admin.py
+++ user/admin.py
@@ -3,7 +3,33 @@ from user.models import User as UserModel
 from user.models import UserProfile as UserProfileModel
 from user.models import Hobby as HobbyModel
 
-# Register your models here.
-admin.site.register(UserModel)
+# 커스텀 admin import
+from django.contrib.auth.admin import UserAdmin as BaceUserAdmin
+
+'''
+    커스텀 admin
+    UserAdmin 테이블을 만들어 
+    UserModel에 상속 시켜줌
+'''
+class UserAdmin(BaceUserAdmin):
+    list_display = ('id', 'username', 'fullname', 'email')
+    list_display_links = ('username', )
+    list_filter = ('username', )
+    search_fields = ('username', 'email', )
+
+    fieldsets = (
+        ("info", {'fields': ('username', 'password', 'email', 'fullname', 'join_date',)}),
+        ('permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+    filter_horizontal = []
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('username', 'join_date', )
+        else:
+            return ('join_date', )
+        
+
+admin.site.register(UserModel, UserAdmin)
 admin.site.register(UserProfileModel)
 admin.site.register(HobbyModel)
